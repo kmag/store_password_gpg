1a58008dc958dfb255f680f2f8178f31ab518a37
Karl Magick <karl@magdsick.com>
2025-02-23 20:48:52 +0000

    Ensure one character from each character class
    This change ensures that at least one character from each character class appears in the password.   This slightly reduces entropy, but better satisfies site complexity requirements

--- store_password_gpg.py
+++ store_password_gpg.py
@@ -16,8 +16,9 @@
 
 # Site configuration
 DEFAULT_EMAIL_DOMAIN = 'gmail.com'
-GPG_PATH = 'gpg'
+GPG_PATH = 'gpg1'
 MIN_BITS=40
+DEFAULT_BITS=96
 
 import argparse
 import bz2
@@ -30,26 +31,47 @@ import sys
 import string
 import time
 
-ALPHABET_32 = string.digits + 'abcdefghjkmnpqrstvwxyz'
-ALPHABET_36 = string.digits + string.ascii_lowercase
-ALPHABET_62 = string.ascii_letters + string.digits
-ALPHABET_64 = ALPHABET_62 + '_-'
-ALPHABET_68 = ALPHABET_64 + '.!?/'
-ALPHABET_71 = ALPHABET_64 + '#@$?.!/'
-ALPHABET_84 = ALPHABET_62 + string.punctuation
 
-ALL_ALPHABETS = [string.digits, string.ascii_lowercase, string.ascii_letters, ALPHABET_32, ALPHABET_36, ALPHABET_62, ALPHABET_64, ALPHABET_68,
-  ALPHABET_71, ALPHABET_84]
-DEFAULT_ALPHABETS = (ALPHABET_62, ALPHABET_71, ALPHABET_71, ALPHABET_71, ALPHABET_84)
+ALPHABET_32 = (string.digits, 'abcdefghjkmnpqrstvwxyz')
+ALPHABET_36 = (string.digits, string.ascii_lowercase)
+ALPHABET_62 = ALPHABET_36 + (string.ascii_uppercase,)
+ALPHABET_64 = ALPHABET_62 + ('_-',)
+ALPHABET_66 = ALPHABET_62 + ('$#*%',)
+ALPHABET_68 = ALPHABET_62 + ('_-.!?$',)
+ALPHABET_71 = ALPHABET_62 + ('_-#@$?.!/',)
+ALPHABET_84 = ALPHABET_62 + (string.punctuation,)
+
+ALL_ALPHABETS = [string.digits, string.ascii_lowercase, string.ascii_letters, ALPHABET_32, ALPHABET_36, ALPHABET_62, ALPHABET_64,
+  ALPHABET_66, ALPHABET_68, ALPHABET_71, ALPHABET_84]
+DEFAULT_ALPHABETS = (ALPHABET_62, ALPHABET_66, ALPHABET_71, ALPHABET_71, ALPHABET_71, ALPHABET_84)
+
+TO_LOG2 = 1 / log(2)
+
+def log2(n):
+    return log(n) * TO_LOG2
 
 def lazy_len(s):
-  return s.__len__()
+  if type(s) == tuple:
+      return sum(map(len, s))
+  return s.__len__()  # len() type-checks that an int is returned
+
+def strict_len(s):
+  if type(s) == tuple:
+      return sum(map(len, s))
+  elif type(s) == WordList:
+      s.result()
+  return len(s)
 
 def base_dir():
+  result = os.getenv('KMAG_STOREPASSWORD_DIR')
+  if result != None:
+    return result
   if os.name == 'posix':
     return os.path.join(os.getenv('HOME'), 'Documents', 'Passwords')
   else:
     return os.path.join(os.getenv('CSIDL_MYDOCUMENTS'), 'Passwords')
+
+
 
 def get_bits(n, generator = os.urandom):
   '''Returns a long integer suitable for generating a password with at least n bits of entropy'''
@@ -60,24 +82,46 @@ def get_bits(n, generator = os.urandom):
     result = 256 * result + x
   return result
 
+def shuffle(buf, generator = os.urandom):
+    length = len(buf)
+    seed = get_bits(log2(math.factorial(length)), generator)
+    for i in range(length, 1, -1):
+        j = seed % i
+        seed //= i
+        k = i - 1
+        if j != k:
+          buf[j], buf[k] = buf[k], buf[j]
+
 def create_password(bits, alphabet = ALPHABET_71, generator = os.urandom):
-  alphabet[0] # Force lazy values
+  if type(alphabet) == tuple:
+    char_classes = alphabet
+    alphabet = ''.join(alphabet)
+  else:
+    char_classes = ()
+    alphabet[0] # Force lazy values
   alphabet_size = len(alphabet)
   length = ceil(log(1<<bits) / log(alphabet_size))
-  bits = ceil(log(alphabet_size**length) / log(2))
+  bits = ceil(log2(alphabet_size**length))
   seed = get_bits(bits, generator)
+  result = []
   if type(alphabet) == str:
-    result = ''
-    while len(result) < length:
-      result += alphabet[seed % alphabet_size]
-      seed //= alphabet_size
-    return result
-  else:
     result = []
+    limit = 1 << bits
+    combinations = 1
+    for c in char_classes:
+      count = len(c)
+      result.append(c[seed % count])
+      seed //= count
+      combinations *= count
+    while combinations < limit:
+      result.append(alphabet[seed % alphabet_size])
+      seed //= alphabet_size
+      combinations *= alphabet_size
+  else:
     while len(result) < length:
       result.append(alphabet[seed % alphabet_size])
       seed //= alphabet_size
-    return ' '.join(result)
+  return ''.join(result)
 
 def defaults():
   config_path = os.path.join(base_dir(),'config.json')
@@ -88,8 +132,6 @@ def defaults():
     config = {}
   if not 'email' in config:
     config['email'] = '{0}@{1}'.format(os.getenv('USER'), DEFAULT_EMAIL_DOMAIN)
-  if not 'user' in config:
-    config['user'] = config['email']
   if not 'keys' in config:
     config['keys'] = []
   if 'key' in config:
@@ -97,10 +139,6 @@ def defaults():
     del config['key']
   if len(config['keys']) == 0:
     config['keys'].append(config['email'])
-  if not 'alphabet' in config:
-     config['alphabet'] = None 
-  if not 'bits' in config:
-    config['bits'] = 96
   if not 'wordlist' in config:
     config['wordlist'] = 'wordlist.txt.bz2'
   # wordlist in the config doesn't make sense as a path relative to cwd specified in the config
@@ -175,7 +213,7 @@ class WordList:
 
   def result(self):
     if self.__words == None:
-      print('Trying to load ' + self.__path)
+      print('Trying to load ' + self.__path, file=sys.stderr)
       with bz2.BZ2File(self.__path) as wordlist:
         self.__words = [line.decode('utf8').strip() for line in wordlist.readlines()]
       self.__path = None
@@ -188,18 +226,20 @@ def try_load_wordlist(path):
     ALL_ALPHABETS.append(words)
     ALL_ALPHABETS.append(words) # Make pass-phrases thee times as likely
 
-def shuffle_alphabets(alphabet_len, loop):
-  if alphabet_len != None and alphabet_len > 0:
-    if alphabet_len < 100:
-      result = [x for x in list(ALL_ALPHABETS) if lazy_len(x) == alphabet_len]
-    else:
-      result = [x for x in list(ALL_ALPHABETS) if lazy_len(x) >= alphabet_len]
-    if len(result) == 0:
-      raise ValueError('No known alphabets of length {0}'.format(alphabet_len))
+def shuffle_alphabets(min_alphabet_len, alphabet_len, loop):
+  if alphabet_len is not None:
+    result = [x for x in ALL_ALPHABETS if lazy_len(x) == alphabet_len]
+    if len(result) == 0 and min_alphabet_len is not None and min_alphabet_len < alphabet_len:
+      result = [x for x in ALL_ALPHABETS if lazy_len(x) >= min_alphabet_len]
+  elif min_alphabet_len is not None:
+    result = [x for x in ALL_ALPHABETS if lazy_len(x) >= min_alphabet_len]
   elif loop:
     result = list(ALL_ALPHABETS)
   else:
     result = list(DEFAULT_ALPHABETS)
+  if len(result) == 0:
+    lengths = map(str, sorted(set(strict_len(x) for x in ALL_ALPHABETS)))
+    raise ValueError(f'No known alphabets of length {alphabet_len}, available: {",".join(lengths)}')
   random.shuffle(result)
   return result
 
@@ -207,23 +247,24 @@ if __name__ == '__main__':
   config = defaults()
   parser = argparse.ArgumentParser(description='Generate a secure password')
   parser.add_argument('domain')
-  parser.add_argument('--user', default=config['user'])
-  parser.add_argument('--email', default=config['email'])  
-  parser.add_argument('--key', action='append', dest='keys', help='GPG key to use to store random passwords', default=config['keys'])  
-  parser.add_argument('--alphabet', type=int, help='Number of characters in the alphabet to be used', default=config['alphabet'])  
-  parser.add_argument('--wordlist', help='Path to a bzip2 compressed list of words, one word per line', default=config['wordlist'])  
-  parser.add_argument('--bits', type=int, help='password securty level, in bits of entropy', default=config['bits'])
+  parser.add_argument('--user', default=config.get('user'))
+  parser.add_argument('--email', default=config.get('email'))
+  parser.add_argument('--key', action='append', dest='keys', help='GPG key to use to store random passwords', default=config['keys'])
+  parser.add_argument('--alphabet', type=int, help='Number of characters in the alphabet to be used', default=config.get('alphabet'))
+  parser.add_argument('--min_alphabet', type=int, help='Minimum number of characters in the alphabet to be used', default=config.get('min_alphabet'))
+  parser.add_argument('--wordlist', help='Path to a bzip2 compressed list of words, one word per line', default=config['wordlist'])
+  parser.add_argument('--bits', type=int, help='password securty level, in bits of entropy', default=config.get('bits', DEFAULT_BITS))
   parser.add_argument('--loop', action='store_true', help='loop forever generating passwords until interrupted')
   parser.add_argument('--verbose', action='store_true', help='verbose output')
   parser.add_argument('--note', action='append', dest='notes', help='Note to be added to encrypted data')
   args = parser.parse_args()
   if args.bits < MIN_BITS:
     parser.print_help()
-    print('\n\nPasswords must be at least {0} bits strong\n'.format(max(64, MIN_BITS))) # lie about the limit   
+    print('\n\nPasswords must be at least {0} bits strong\n'.format(max(64, MIN_BITS))) # lie about the limit
     sys.exit(1)
   gpg_file = os.path.join(base_dir(), args.domain + '.gpg')
   try_load_wordlist(args.wordlist)
-  alphabets = shuffle_alphabets(args.alphabet, args.loop)
+  alphabets = shuffle_alphabets(args.min_alphabet, args.alphabet, args.loop)
   if args.domain == '':
     if args.loop:
       try:
@@ -258,13 +299,10 @@ if __name__ == '__main__':
         print('\n')
     else:
       password = create_password(args.bits, alphabets[0])
-    # Always include domain at the top of the file to prevent domain-switching attack
-    # whene an attacker who can't break the encryption switches files and tricks the
-    # user into using the password for a valuable site on a non-valuable site controlled
-    # by the attacker.
-    msg = 'domain: {0}\nusername:  {1}\npassword:  {2}'.format(args.domain, args.user, password)
-    if args.email != args.user:
-      msg += '\nemail:  ' + args.email
+    if args.user == args.email:
+      msg = 'domain: {0}\nusername:  {1}\npassword:  {2}'.format(args.domain, args.user, password)
+    else:
+      msg = 'domain: {0}\nusername:  {1}\npassword:  {2}\nemail:  {3}'.format(args.domain, args.user, password, args.email)
     if args.notes:
       msg += '\nNotes:\n    {0}'.format('\n\n    '.join(args.notes))
     cmd = '{0} -e -r "{1}" --output "{2}"'.format( GPG_PATH, '" -r "'.join(args.keys), gpg_file)
